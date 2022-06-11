1e78c0016f7b170588893e581f826ecbb4c66b9a
eloilsonfontenele2@gmail.com <eloilsonfontenele2@gmail.com>
2022-06-11 14:59:56 -0300

    terminado a versão mobile, foi adicionado novas animações, estilos e javascript

--- src/js/gameScript.js
+++ src/js/gameScript.js
@@ -1,27 +1,55 @@
-const mario = document.querySelector('.mario')
-const pipe = document.querySelector('.pipe')
-const score = document.querySelector('.score-point')
-let base = 0
-const jump = () => {
-    mario.classList.add('jump')
+function run() {
+  const mario = document.querySelector('.mario')
+  const pipe = document.querySelector('.pipe')
+  const score = document.querySelector('.score-point')
+  const preLopad = document.querySelector('.preload')
+  let base = 0
+  let firstGame = true
+  let device = 2
 
+  const globalJump = () => {
+    firstGame = false
+
+    preLopad.style.display = 'none'
+    mario.classList.add('jump')
     setTimeout(() => {
-        mario.classList.remove('jump')
-    }, 500
-    )
+      mario.classList.remove('jump')
+    }, 500)
     base = base + 1
     score.innerHTML = base
-}
 
-const loop = setInterval(() => {
+  }
 
-    const pipePosition = pipe.offsetLeft
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+  const mobileEngine = () => {
+    device = 1
+    globalJump()
+  }
 
+  const desktopEngine = () => {
+    device = 0
+    globalJump()
+  }
+  const Loop = setInterval(() => {
+    let pipePosition = pipe.offsetLeft
+    let marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+    if (device == 1) {
+      if (!firstGame) {
+        if (pipePosition < 105 && pipePosition > 0 && marioPosition < 60) {
 
+          pipe.style.animation = 'none'
+          pipe.style.left = `${pipePosition}px`
 
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-
+          mario.style.animation = 'none'
+          mario.style.bottom = `${marioPosition}px`
+          mario.src = 'src/images/game-over.png'
+          mario.style.width = '70px'
+          mario.style.marginLeft = '20px'
+          clearTimeout(Loop)
+        }
+      }
+    }
+    if (device == 0) {
+      if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = 'none'
         pipe.style.left = `${pipePosition}px`
 
@@ -31,9 +59,12 @@ const loop = setInterval(() => {
         mario.style.width = '80px'
         mario.style.marginLeft = '20px'
         clearInterval(loop)
+      }
     }
+  }, 10)
 
-}, 10)
+  document.addEventListener("touchstart", mobileEngine);
+  document.addEventListener('onclick', desktopEngine);
+}
 
-document.addEventListener('keydown', jump)
-console.log(base)
+run()
