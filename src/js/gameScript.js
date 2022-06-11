d22d933bb11b7a0a30dac2888cbc8fe6863d6e6c
elo1lson <eloilsonfontenele2@gmail.com>
2022-06-11 20:26:20 -0300

    Novas imagens, novas animações, e remodelagem do css para desktop

--- src/js/gameScript.js
+++ src/js/gameScript.js
@@ -1,70 +1,50 @@
-function run() {
-  const mario = document.querySelector('.mario')
-  const pipe = document.querySelector('.pipe')
-  const score = document.querySelector('.score-point')
-  const preLopad = document.querySelector('.preload')
-  let base = 0
-  let firstGame = true
-  let device = 2
 
-  const globalJump = () => {
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
+const score = document.querySelector('.score-point')
+const preLopad = document.querySelector('.preload')
+let base = 0
+let firstGame = true
+let device = 2
+
+const globalJump = () => {
     firstGame = false
 
     preLopad.style.display = 'none'
     mario.classList.add('jump')
     setTimeout(() => {
-      mario.classList.remove('jump')
+
+        mario.classList.remove('jump')
     }, 500)
-    base = base + 1
-    score.innerHTML = base
 
-  }
+    setTimeout(() => {
+        base = base + 1
+        score.innerHTML = base
 
-  const mobileEngine = () => {
-    device = 1
-    globalJump()
-  }
-
-  const desktopEngine = () => {
-    device = 0
-    globalJump()
-  }
-  const Loop = setInterval(() => {
-    let pipePosition = pipe.offsetLeft
-    let marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
-    if (device == 1) {
-      if (!firstGame) {
-        if (pipePosition < 105 && pipePosition > 0 && marioPosition < 60) {
-
-          pipe.style.animation = 'none'
-          pipe.style.left = `${pipePosition}px`
-
-          mario.style.animation = 'none'
-          mario.style.bottom = `${marioPosition}px`
-          mario.src = 'src/images/game-over.png'
-          mario.style.width = '70px'
-          mario.style.marginLeft = '20px'
-          clearTimeout(Loop)
-        }
-      }
-    }
-    if (device == 0) {
-      if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-        pipe.style.animation = 'none'
-        pipe.style.left = `${pipePosition}px`
-
-        mario.style.animation = 'none'
-        mario.style.bottom = `${marioPosition}px`
-        mario.src = 'src/images/game-over.png'
-        mario.style.width = '80px'
-        mario.style.marginLeft = '20px'
-        clearInterval(loop)
-      }
-    }
-  }, 10)
-
-  document.addEventListener("touchstart", mobileEngine);
-  document.addEventListener('onclick', desktopEngine);
+    }, 1400)
 }
 
-run()
+const Loop = setInterval(() => {
+    let pipePosition = pipe.offsetLeft
+    let marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+    if (!firstGame) {
+
+        if (pipePosition < 105 && pipePosition > 0 && marioPosition < 60) {
+
+            pipe.style.animation = 'none'
+            pipe.style.left = `${pipePosition}px`
+
+            mario.style.animation = 'none'
+            mario.style.bottom = `${marioPosition}px`
+            mario.src = 'src/images/game-over.png'
+            mario.style.width = '70px'
+            mario.style.marginLeft = '20px'
+            clearTimeout(Loop)
+
+        }
+    }
+}, 10)
+
+document.addEventListener("touchstart", globalJump);
+document.addEventListener('keydown', globalJump);
+
