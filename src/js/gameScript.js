bde73f2972d691bf627d6a00f0c7b0ad0e902561
eloilsonfontenele2@gmail.com <eloilsonfontenele2@gmail.com>
2022-06-11 21:44:22 -0300

    Organizando os arquivos

--- src/js/gameScript.js
+++ src/js/gameScript.js
@@ -1,50 +1,62 @@
+'use strict'
 
-const mario = document.querySelector('.mario')
-const pipe = document.querySelector('.pipe')
-const score = document.querySelector('.score-point')
-const preLopad = document.querySelector('.preload')
-let base = 0
-let firstGame = true
-let device = 2
+//Global variables
+function gameRun() {
+  const mario = document.querySelector('.mario');
+  const pipe = document.querySelector('.pipe');
+  const score = document.querySelector('.score-point');
+  const preLopad = document.querySelector('.preload');
 
-const globalJump = () => {
-    firstGame = false
+  let base = 0;
+  let firstGame = true;
+  let device = 2;
 
-    preLopad.style.display = 'none'
-    mario.classList.add('jump')
+  const globalJump = () => {
+    firstGame = false;
+
+    preLopad.style.display = 'none';
+    mario.classList.add('jump');
+
     setTimeout(() => {
-
-        mario.classList.remove('jump')
+      mario.classList.remove('jump');
     }, 500)
 
+    //Score test loop
+
     setTimeout(() => {
-        base = base + 1
-        score.innerHTML = base
+      base = base + 1;
+      score.innerHTML = base;
 
     }, 1400)
-}
+  }
+  //Game loop
 
-const Loop = setInterval(() => {
-    let pipePosition = pipe.offsetLeft
-    let marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+  const Loop = setInterval(() => {
+    let pipePosition = pipe.offsetLeft;
+    let marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
     if (!firstGame) {
 
-        if (pipePosition < 105 && pipePosition > 0 && marioPosition < 60) {
+      //Looser checker
 
-            pipe.style.animation = 'none'
-            pipe.style.left = `${pipePosition}px`
+      if (pipePosition < 105 && pipePosition > 0 && marioPosition < 60) {
 
-            mario.style.animation = 'none'
-            mario.style.bottom = `${marioPosition}px`
-            mario.src = 'src/images/game-over.png'
-            mario.style.width = '70px'
-            mario.style.marginLeft = '20px'
-            clearTimeout(Loop)
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
 
-        }
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+        mario.src = 'src/images/game-over.png';
+        mario.style.width = '70px';
+        mario.style.marginLeft = '20px';
+
+        clearTimeout(Loop);
+
+      }
     }
-}, 10)
+  }, 10)
 
-document.addEventListener("touchstart", globalJump);
-document.addEventListener('keydown', globalJump);
+  document.addEventListener("touchstart", globalJump);
+  document.addEventListener('keydown', globalJump);
+}
 
+gameRun()
